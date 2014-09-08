4b8ef109ff012ad0257f944805ca3dafe4d5cc3d
Samuel Loup <samuel.loup@gmail.com>
2014-09-08 22:43:59 +0200

    welcome buds

---
+++ server/controllers/buds.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/**
+ * Posts controller for serving user posts.
+ */
+
+var route = require('koa-route'),
+    parse = require('co-body'),
+    mongo = require('../config/mongo'),
+    ws = require('../config/ws'),
+    ObjectID = mongo.ObjectID;
+
+// register koa routes
+exports.init = function (app) {
+  app.use(route.get('/api/buds', listBuds));
+  app.use(route.post('/api/buds', createBud));
+  app.use(route.post('/api/buds/:budId/comments', createComment));
+};
+
+/**
+ * Lists last 15 posts with latest 15 comments in them.
+ */
+function *listBuds()
+{
+  var buds = yield mongo.buds.find(
+      {},
+      {comments: {$slice: -15 /* only get last x many comments for each post */}},
+      {limit: 15, sort: {_id: -1}} /* only get last 15 posts by last updated */).toArray();
+
+  buds.forEach(function (bud)
+  {
+    bud.id = bud._id;
+    delete bud._id;
+  });
+
+  this.body = buds;
+}
+
+/**
+ * Saves a new post in the database after proper validations.
+ */
+function *createBud()
+{
+  // it is best to validate bud body with something like node-validator here, before saving it in the database..
+  var bud  = yield parse(this);
+  bud.from = this.user;
+  bud.createdTime = new Date();
+  var results = yield mongo.buds.insert(bud);
+
+  this.status = 201;
+  this.body = results[0]._id.toString(); // we need .toString() here to return text/plain response
+
+  // now notify everyone about this new post
+  bud.id = bud._id;
+  delete bud._id;
+  ws.notify('buds.created', bud);
+}
+
+/**
+ * Appends a new comment to a given post.
+ * @param postId - Post ID.
+ */
+function *createComment(budId)
+{
+  budId         = new ObjectID(budId);
+  var comment   = yield parse(this);
+  var commentId = new ObjectID();
+
+  // update post document with the new comment
+  comment = {_id: commentId, from: this.user, createdTime: new Date(), message: comment.message};
+  var result = yield mongo.buds.update(
+      {_id: budId},
+      {$push: {comments: comment}}
+  );
+
+  this.status = 201;
+  this.body = commentId.toString(); // we need .toString() here to return text/plain response
+
+  // now notify everyone about this new comment
+  comment.id = comment._id;
+  comment.budId = budId;
+  delete comment._id;
+  ws.notify('buds.comments.created', comment);
+}
